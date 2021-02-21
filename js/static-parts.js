7247b4a0b89ffb157f979a28ef79fad1a6e4b423
Matt Price <matt.price@utoronto.ca>
2021-02-21 15:08:52 -0500

    static-parts: reintroducem enu, also cleanup

--- js/static-parts.js
+++ js/static-parts.js
@@ -1,24 +1,25 @@
-// use this file to set constant elements that don't change
-// across individual pages
+// A tiny script to generate header & footer
+// separated out to make code easier to read
 
 let menuEntries = [
-  {text: "Map",
+  {text: "Info",
    link: 'index.html'},
+  {text: "Mapping Exercise",
+   link: 'spatial-history/index.html'}
 ];
 
+// set this globally as we're using it twice 
+let author = $('meta[name=author]')[0].content || '';
 
-let authorName='YOUR NAME HERE',
-    footerHtml=`Map Exercise by ${authorName}, based on code from  <a href="https://github.com/titaniumbones/UTSC-map-exercise">this repository</a>`;
-
-
+// make menu from entries
 function makeMenu (items= menuEntries) {
   let prefix = '',
       html = '',
       basedir = window.location.pathname.split(/\//g).splice(-2)[0]; // check which dir we're in
-    // console.log("checking location");
-    // console.log(basedir);
-    // console.log(window.location.pathname)
-    if (! (basedir === 'advanced-topics' || basedir === "")) {
+  // console.log("checking location");
+  // console.log(basedir);
+  // console.log(window.location.pathname)
+  if (! (basedir === 'advanced-topics' || basedir === "")) {
     prefix = '../';
   }
   for (let i of items) {
@@ -28,18 +29,22 @@ function makeMenu (items= menuEntries) {
   return html
 }
 
-function makeHeader (items = menuEntries) {
-  let title = `<a class="navbar-brand pull-right" href="#">${document.title}</a>`,
-      menu = makeMenu (items);
-  $('header.nav').append( title + menu);
+// get author and title from tags in <head>
+function makeHeader () {
+  let authorString = author ? `| ${author}` : '',
+      title = `<a class="navbar-brand pull-right" href="#">${document.title} ${authorString}</a>`,
+      menu=makeMenu();
+  $('header.nav').append( title + menu );
 }
 
-function makeFooter (html) {
-  $('footer#page-footer').html(`<main>${html}</main>`);
+// pull author again
+function makeFooter () {
+  let authorString = author ? ` by ${author}` : '',
+      footerHtml=`Map Exercise${authorString}, based on code from  <a href="https://github.com/titaniumbones/UTSC-map-exercise">this repository</a>`;
+  $('footer#page-footer').html(`<main>${footerHtml}</main>`);
 }
 
-makeHeader (menuEntries);
-makeFooter (footerHtml);
-
-
-
+// would be cleaner to wrap everything in a single function; oh well, next time!
+// the perfect is the enemy of the good.
+makeHeader ();
+makeFooter ();
